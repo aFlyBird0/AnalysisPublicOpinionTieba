45f070566f6a59478470878c3654b3e2dfd193a2
dingjianhub <43640148+dingjianhub@users.noreply.github.com>
2019-06-01 09:26:21 +0800

    bugs fixed
    1）修复了前端搜索按钮按下无反馈的问题

---
+++ tiebaSpider/flask_tiebaSearch_demo.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, render_template, redirect, url_for, make_response
+from tiebaSpider import tieba3, spiderOnlyFirstFloorAdvance
+from SentimentAnalysis import Analysis
+from flask_bootstrap import Bootstrap
+import time
+import json
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+
+users = []
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('simple_index.html', says=users)
+
+    else:
+        users.clear()  # 清除输入框中的内容，方便下一次查询时输入
+
+        context = request.form.get('context')  # 获取需要查询的文本关键字
+        page = request.form.get('page')  # 获取需要查询的帖子页面数
+        print(context)
+        print(page)
+        result = spiderOnlyFirstFloorAdvance.get_list_first_floor_advance(page_want=int(page), keyword=context)
+        for res in result:
+            print(res['firstFloorContent'])
+            emotion = Analysis.analysis_Dict(res['title'])['items'][0]['positive_prob']  # 调用百度的”情感分析API“，返回文本的情感极性值
+            if emotion >= 0.800:  # 对返回的值进行一个登记划分，以 20% 为一档
+                emotion_type = "非常积极"
+            elif 0.600 <= emotion < 0.800:
+                emotion_type = "较为积极"
+            elif 0.400 <= emotion < 0.600:
+                emotion_type = "中性情感"
+            elif 0.200 <= emotion < 0.400:
+                emotion_type = "较为消极"
+            elif emotion < 0.200:
+                emotion_type = "非常消极"
+            users.append(
+                {"text": res['firstFloorContent'], "title": res['title'], "href": res['href'], "emotion": emotion_type})
+
+        return redirect(url_for('index'))
+
+
+@app.route('/show', methods=['GET', 'POST'])  # 定义路由(Views)，可以理解为定义页面的URL
+def show():
+    # page = int(request.args.get('page'))
+    # keyword = request.args.get('keyword')
+    # result = tieba3.gogogoStringBr(page, keyword)
+    # return render_template('show.html', page=page, keyword=keyword, result=result)
+    # return render_template('info.html', page=page, keyword=keyword, result=result)
+    # result = spiderOnlyFirstFloorAdvance.get_list_first_floor_advance(page_want=1, keyword="杭州电子科技大学")
+    # return render_template('tieba_monitor_index.html', result=result)
+    return render_template('tieba_monitor_index.html', )
+
+
+@app.route('/info?page=<string:page>&keyword=<string:keyword>', methods=['GET', 'POST'])
+def get_info():
+    page = request.args.get('page')
+    keyword = request.args.get('keyword')
+    result = spiderOnlyFirstFloorAdvance.get_list_first_floor_advance(page_want=page, keyword=keyword)
+    return result
+
+
+if __name__ == '__main__':
+    app.run(debug=True, )
--- tiebaSpider/static/my_js/index.js
+++ tiebaSpider/static/my_js/index.js
@@ -4,65 +4,64 @@ $(document).ready(function () {
 
     // id为searchInfo的按钮按下触发searchInfo()方法
     $("#searchInfo").click(function () {
-        keyword = $("#keyword").val();
-        searchInfo(keyword);
+        searchInfo(
+            page = $("#page").val(),
+            keyword = $("#keyword").val());
     });
 
 });
 
+
 // 在按下enter键的时候就搜索
 $(document).keyup(function (event) {
     if (event.keyCode == 13) {
-        searchInfo($("#keyword").val());
+        searchInfo(
+            page = $("#page").val(),
+            keyword = $("#keyword").val());
     }
 });
 
-function searchInfo(key) {
+function searchInfo(page, keyword) {
     // 首先清空result中的内容以便内容填入
     $("#result").empty();
-    $.getJSON({
-        url: "http://localhost:8080/info?page=1&keyword=杭州电子科技大学",
+    $.ajax({
+        url: "http://localhost:5000/info?page=" + page + "&keyword=" + keyword,
+        type: "GET",
+        dataType: "json",
         success: function (result) {
-            // 获取返回的数据中我们需要的部分
-            res = result.response.toJSON();
-            // 利用for插入每一个结果
-            if (res.length) {
-                for (i = 0; i < res.length; i++) {
-                    // 将返回的结果包装成HTML
-                    resultItem =
-                        `
+            // 循环输出json对象result中的键值对
+            $.each(result, function (i) {
+                // 将返回的结果包装成HTML
+                resultItem = `
                         <div class='col-md-12 mb-4'>
                             <div class='card mb-12 shadow-sm'>
                                 <div class='card-body'>
-                                    <h5>` +
-                        res[i].firstFloorContent +
-                        ` <small style='margin-left: 10px'>` +
-                        res[i].href +
-                        `</small> <small style='margin-left: 10px'>` +
-                        res[i].title +
-                        `</small></h5>
-                                    <p class='text-muted' style='margin-bottom: 0.5em'>` +
-                        res[i].href +
-                        `</p>
-                                    <p class='card-text'>` +
-                        res[i].abstract +
-                        `</p>
+                                    <h5>` + res[i].firstFloorContent + ` 
+                                        <small style='margin-left: 10px'>` + res[i].title + `</small> 
+                                        <small style='margin-left: 10px'>` + res[i].href + `</small>
+                                    </h5>
+                                    <p class='text-muted' style='margin-bottom: 0.5em'>` + res[i].unit + `</p>
+                                    <p class='card-text'>` + res[i].abstract + `</p>
                                 </div>
                             </div>
                         </div>
                     `;
-                    // 插入HTML到result中
-                    $("#result").append(resultItem);
-                }
+                // 插入HTML到result中
+                $("#result").append(resultItem);
+            });
 
-                // 搜索完以后让搜索框移上去，带有动画效果
-                $("section.jumbotron").animate({
-                    margin: "0"
-                });
-                // 显示搜索结果的部分
-                $("#resultSection").show();
-            
-            }
+            // 搜索完以后让搜索框移上去，带有动画效果
+            $("section.jumbotron").animate({
+                margin: "0"
+            });
+
+            // 显示搜索结果的部分
+            $("#resultSection").show();
+
+            // error: function (error) {
+            //     console.log(error)
+            // }
+
         }
     });
 }
